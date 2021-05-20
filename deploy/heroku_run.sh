0b7499258ec9a4790dc7268e65fa87c96be49c92
Gabriele Sarti <gabriele.sarti996@gmail.com>
2021-05-20 15:16:06 +0200

    Added google credentials in Heroku run

--- deploy/heroku_run.sh
+++ deploy/heroku_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
+if [ -z ${GOOGLE_CREDENTIALS+x} ]; then echo $GOOGLE_CREDENTIALS > $GOOGLE_APPLICATION_CREDENTIALS; fi
 label-studio --host ${HOST:-""} --port ${PORT} --username ${USERNAME} --password ${PASSWORD}
